df216d110b934791cb53392efbf1f98fa3687bfe
Jovan Shandro <shandrojovan@gmail.com>
2020-04-06 21:59:07 +0200

    Create basic component structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Sudoku Solver</div>;
+};
+
+export default Header;
---
+++ src/components/Square.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Square = () => {
+  return <div>S</div>;
+};
+
+export default Square;
