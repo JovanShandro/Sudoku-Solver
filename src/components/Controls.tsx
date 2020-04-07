57c4fda3560f6a41c9a0e875ab2bab5b29fe02d6
Jovan Shandro <shandrojovan@gmail.com>
2020-04-08 01:41:11 +0200

    Add import functionality

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { pipe, split, map, splitEvery, take } from "ramda";
 import "../css/Controls.css";
 
 interface Props {
@@ -12,10 +13,33 @@ const Controls: React.FC<Props> = ({
   setImportedBoard,
   solveSudoku
 }) => {
+  const handleFile = files => {
+    if (files[0]) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const importedBoard = pipe(
+          split("\n"),
+          map(row => splitEvery(1)(row.toString().replace(/[ ]/g, ""))),
+          take(9)
+        )(reader.result);
+        setImportedBoard(importedBoard);
+      };
+      reader.readAsText(files[0]);
+    }
+  };
+
   return (
     <div>
       <button onClick={clearBoard}>Clear</button>
-      <button>Import</button>
+      <div>
+        <label htmlFor="file">Import</label>
+        <input
+          id="file"
+          type="file"
+          style={{ visibility: "hidden" }}
+          onChange={e => handleFile(e.target.files)}
+        />
+      </div>
       <button>Solve</button>
     </div>
   );
