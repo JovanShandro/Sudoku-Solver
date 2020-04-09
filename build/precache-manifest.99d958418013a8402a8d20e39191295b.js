ec5ad3d00b81219d6a4cfcc7b5d344b230c0f7a8
Jovan Shandro <shandrojovan@gmail.com>
2020-04-09 22:13:26 +0200

    Build project

---
+++ build/precache-manifest.99d958418013a8402a8d20e39191295b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22032b4aee3973108c9587c98088fb83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10831c4b9edcde23aca5",
+    "url": "/static/css/main.b665f071.chunk.css"
+  },
+  {
+    "revision": "f52ec0dd598e9749f8f8",
+    "url": "/static/js/2.41d2228a.chunk.js"
+  },
+  {
+    "revision": "505a6f6bdacd226f93a956d35104ccd6",
+    "url": "/static/js/2.41d2228a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10831c4b9edcde23aca5",
+    "url": "/static/js/main.fb5aa9a5.chunk.js"
+  },
+  {
+    "revision": "09c24389187c1814827a",
+    "url": "/static/js/runtime-main.6d55be67.js"
+  }
+]);
