a357135dcecc8b724df255696773b71ef85e93cf
Jovan Shandro <shandrojovan@gmail.com>
2020-04-09 22:07:24 +0200

    Fix minor styling issues.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,20 +13,21 @@ const Header = () => {
         <span className="tooltip">
           <span className="tooltip--text"> this</span>
           <span className="tooltip--dropdown">
-            1 2 3&nbsp; 4 5 6&nbsp; 7 8 9 <br />
-            4 5 6&nbsp; 7 8 9&nbsp; 1 2 3 <br />
-            7 8 9&nbsp; 1 2 3&nbsp; 4 5 6 <br />
+            1 2 3&nbsp; 4 5 6&nbsp; 7 0 9 <br />
+            4 0 6&nbsp; 7 8 9&nbsp; 1 2 3 <br />
+            7 8 9&nbsp; 1 2 3&nbsp; 0 5 6 <br />
             <span className="br"></span>
-            2 1 4&nbsp; 3 6 5&nbsp; 8 9 7 <br />
-            3 6 5&nbsp; 8 9 7&nbsp; 2 1 4 <br />
-            8 9 7&nbsp; 2 1 4&nbsp; 3 6 5 <br />
+            2 1 4&nbsp; 3 6 5&nbsp; 0 9 7 <br />
+            3 6 5&nbsp; 8 0 7&nbsp; 2 1 4 <br />
+            8 0 7&nbsp; 2 1 4&nbsp; 3 6 5 <br />
             <span className="br"></span>
-            5 3 1&nbsp; 6 4 2&nbsp; 9 7 8 <br />
-            6 4 2&nbsp; 9 7 8&nbsp; 5 3 1 <br />
-            9 7 8&nbsp; 5 3 1&nbsp; 6 4 2 <br />
+            5 0 1&nbsp; 6 4 2&nbsp; 9 7 8 <br />
+            6 4 2&nbsp; 0 7 8&nbsp; 5 0 1 <br />
+            0 7 8&nbsp; 5 3 1&nbsp; 0 4 2 <br />
           </span>
         </span>
-        ! Use 0 if you want the square to be empty.
+        , otherwise the app will crash! Use 0 if you want the square to be
+        empty.
       </p>
     </div>
   );
